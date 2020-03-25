36a01476b857c474cd6bc621ec12cce9c345158a
Johannes Thomsen <43598221+joha321j@users.noreply.github.com>
2020-03-25 11:48:13 +0100

    Begin trying to solve triangle number problem.

---
+++ Project Euler/Project Euler/HighlyDivisibleTriangleNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+
+namespace Project_Euler
+{
+    public class HighlyDivisibleTriangleNumber
+    {
+        public static long CalculateTriangleNumber(int triangleNumber)
+        {
+            long result = 1;
+
+            for (int i = 2; i <= triangleNumber; i++)
+            {
+                result += i;
+            }
+
+            return result;
+        }
+
+        public static int CalculateNumberOfDivisibles(int triangle)
+        {
+            List<int> primeFactors = PrimeFactorization.Factorize(triangle);
+
+            return 2;
+        }
+    }
+}
---
+++ Project Euler/ProjectEulerUnitTest/HighlyDivisibleTriangleNumberTestClass.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project_Euler;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEulerUnitTest
+{
+    [TestClass]
+    public class HighlyDivisibleTriangleNumberTestClass
+    {
+        [TestMethod]
+        public void GenerateTriangleNumbersTest()
+        {
+            int triangleNumber = 3;
+
+            long thirdTriangleNumber = 6;
+            long calc = HighlyDivisibleTriangleNumber.CalculateTriangleNumber(triangleNumber);
+
+            Assert.AreEqual(thirdTriangleNumber, calc);
+        }
+
+        [TestMethod]
+        public void GenerateTriangleNumbersTestTwo()
+        {
+            int triangleNumber = 10;
+
+            long tenthTriangleNumber = 55;
+
+            long calc = HighlyDivisibleTriangleNumber.CalculateTriangleNumber(triangleNumber);
+
+            Assert.AreEqual(tenthTriangleNumber, calc);
+        }
+
+        [TestMethod]
+        public void GenerateTriangleNumbersSizeTest()
+        {
+            int triangleNumber = 12345678;
+
+            HighlyDivisibleTriangleNumber.CalculateTriangleNumber(triangleNumber);
+        }
+
+        [TestMethod]
+        public void GetNumberOfDivisibles()
+        {
+            int triangle = 3;
+
+            int divisibles = 2;
+
+            int calculated = HighlyDivisibleTriangleNumber.CalculateNumberOfDivisibles(triangle);
+
+            Assert.AreEqual(divisibles, calculated);
+        }
+
+        [TestMethod]
+        public void GetNumberOfDivisblesTwo()
+        {
+            int triangle = 6;
+
+            int divisibles = 4;
+
+            int calculated = HighlyDivisibleTriangleNumber.CalculateNumberOfDivisibles(triangle);
+
+            Assert.AreEqual(divisibles, calculated);
+        }
+
+        [TestMethod]
+        public void GetNumberOfDivisiblesNotOnlyPrimes()
+        {
+            int triangle = 36;
+            int divisibles = 8;
+
+            int calculated = HighlyDivisibleTriangleNumber.CalculateNumberOfDivisibles(triangle);
+
+            Assert.AreEqual(divisibles, calculated);
+        }
+    }
+}
